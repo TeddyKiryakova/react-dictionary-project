1b850f4db6b40b5aed4189efb97a01f6fe22d2a6
TeddyKiryakova <teakiryakova@gmail.com>
2021-07-08 12:29:56 +0100

    `Search input added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import  Dictionary  from "./Dictionary.js";
 
 export default  function App() {
   return (
     <div className="App">
+      <div className="container">
     <h1> Dictionary 📚</h1> 
-     </div>
+    <main>
+      <Dictionary />
+    </main>
+    <footer className="App-footer">
+      Coded by Teodora Kiryakova
+    </footer>
+      </div>
+    </div>
   );
 }
 
